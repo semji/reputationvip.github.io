435efd12e7861da33c996fd6c0239b10b541e524
Olivier Balais <obalais@gmail.com>
2015-02-07 18:39:15 +0000

    Changed theme to Minimal Mistakes
    We needed multi authors blog

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'assets/js/*.js',
-        'assets/js/plugins/*.js',
+        '!assets/js/plugins/*.js',
         '!assets/js/scripts.min.js'
       ]
     },
@@ -79,6 +79,10 @@ module.exports = function(grunt) {
   ]);
   grunt.registerTask('dev', [
     'watch'
+  ]);
+  grunt.registerTask('images', [
+    'imagemin',
+    'svgmin'
   ]);
 
 };
---
+++ index.md
@@ -0,0 +1,9 @@
+---
+layout: home
+excerpt: "At ReputationVIP, we aren't just online reputation experts, we are developpers too!"
+tags: [Jekyll, theme, responsive, blog, template]
+image:
+  feature: reputationvip-photo.jpg
+  credit: ReputationVIP
+  creditlink: http://www.reputationvip.com
+---
