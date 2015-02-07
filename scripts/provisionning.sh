6365b035e158031e4da1a612a390c567fd80ed2e
Olivier Balais <obalais@gmail.com>
2015-02-07 14:06:09 +0000

    Install jekyll dependencies in Vagrant providing

--- scripts/provisionning.sh
+++ scripts/provisionning.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# NodeJs
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs -y
+
 # Ruby / Jekyll
 sudo apt-get update
 sudo apt-get -y install build-essential git ruby1.9.3
 sudo gem install bundler github-pages --no-ri --no-rdoc
 
-# NodeJs
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install nodejs -y
+# Jekyll dependencies
+cd /vagrant
+bundle install
